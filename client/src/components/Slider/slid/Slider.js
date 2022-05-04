7394144a4934cbf02a14c496aa37e6285f3b89fa
aya-sahlouf <104566069+aya-sahlouf@users.noreply.github.com>
2022-05-04 14:52:00 +0000

    slider

---
+++ client/src/components/Slider/slid/Slider.js
@@ -0,0 +1,34 @@
+﻿import React, { useEffect, useState } from "react";
+import SliderContent from "./SliderContent";
+import sliderImage from "./sliderImage";
+import "./slider.css";
+import Button from '@mui/material/Button';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+const len = sliderImage.length - 1;
+
+function Slider(props) {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setActiveIndex(activeIndex === len ? 0 : activeIndex + 1);
+        }, 3000);
+        return () => clearInterval(interval);
+    }, [activeIndex]);
+
+    return (
+        <div className="slider-container">
+            <SliderContent activeIndex={activeIndex} sliderImage={sliderImage} />
+            
+            <div className="slide-text">
+                <p>De vrais bricoleurs pour vous aider
+                    Travaux, déménagements, livraisons & services à domicile</p>
+                <br/> <Button variant="contained" size="large" startIcon={<AddCircleIcon />} href="/services">Demander un service</Button>
+
+            </div>
+        </div>
+    );
+}
+
+export default Slider;
