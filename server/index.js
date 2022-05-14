5afdf25ef48b4fdc2855a8f62f3385e5e6cc092a
messimaha <70766603+messimaha@users.noreply.github.com>
2022-05-14 21:24:43 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
+const postRoutes = require("./routes/posts");
 const authRoutes = require("./routes/auth");
 
 // database connection
@@ -15,6 +16,7 @@ app.use(cors())
 
 // routes
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
  app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 8080;
