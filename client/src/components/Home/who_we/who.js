b739ef2c6be004ce52a619c9ac4003e63c042641
elouazzani yassine <80113396+yassine121212@users.noreply.github.com>
2022-05-14 13:11:43 +0100

    Who We are

---
+++ client/src/components/Home/who_we/who.js
@@ -0,0 +1,38 @@
+
+import "./who.css"
+
+const who = ()=>{
+   return (
+     <>
+       <div className="all">
+           
+           <div className="picdisc">
+               Part 1
+           </div>
+           <div className="disc">
+              <h1>WeGrow</h1>
+                <p>
+                New consumption models have emerged thanks to the rise of new 
+                technologies and changing lifestyles. Jobbing allows tens of 
+                thousands of service providers to provide service in their 
+                neighborhood and earn additional income or launch their independent 
+                activity. Wegrow is a collaborative platform that is part of a 
+                fundamental trend: many sectors are experiencing their technological 
+                revolution such as transport with carpooling, car rental between 
+                individuals or housing. With Wegrow, the job market is undergoing a 
+                revolution! The principle is simple: connect a "poster" (a person 
+                who needs a helping hand for services, kitchen installation, dressing
+                 room assembly, painting, parquet laying, tiling, moving, gardening)
+                  and a "provider" (person with the skills and time available) in 
+                  complete safety. An innovative model with the key to services 
+                  provided for those who do not have the time or the skills to do 
+                  everything themselves, and additional income and developing 
+                  know-how for service providers. They are students, retirees, 
+                  part-time employees, or people wishing to start their own business.                </p>
+           </div>
+       </div>
+
+     </>
+   );
+}
+export default who;
