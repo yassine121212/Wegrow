d8de71342c612ddd5951e7de2c807597d01d7a1f
messimaha <70766603+messimaha@users.noreply.github.com>
2022-05-15 11:28:33 +0200

    Update Slider.js
    slider

--- client/src/components/Home/Slider/slid/Slider.js
+++ client/src/components/Home/Slider/slid/Slider.js
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
 import SliderContent from "./SliderContent";
 import sliderImage from "./sliderImage";
-import Post from "./posts/Posts";
 import "./slider.css";
 import Button from '@mui/material/Button';
+import Post from "./posts/Posts";
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-
 
 const len = sliderImage.length - 1;
 
 function Slider(props) {
     const [activeIndex, setActiveIndex] = useState(0);
-    const [modelvidible, setmodelvidible] = useState(false);
+
     useEffect(() => {
         const interval = setInterval(() => {
             setActiveIndex(activeIndex === len ? 0 : activeIndex + 1);
@@ -20,17 +19,22 @@ function Slider(props) {
     }, [activeIndex]);
 
     return (
+        <div>
         <div className="slider-container">
             <SliderContent activeIndex={activeIndex} sliderImage={sliderImage} />
             
             <div className="slide-text">
                 <p>De vrais bricoleurs pour vous aider
                     Travaux, déménagements, livraisons & services à domicile</p>
-                <br/> <Post className="post" setmodelvidible={setmodelvidible} modelVisi={modelvidible}></Post>
+                <br/> 
 
             </div>
+            
+        </div>
+        <div><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <Post></Post></div>
         </div>
     );
 }
-
 export default Slider;
