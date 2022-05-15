70f8c9b9b6efde2afecf5097b3980c1a107f9851
messimaha <70766603+messimaha@users.noreply.github.com>
2022-05-15 19:11:36 +0200

    Update index.js
    index

--- server/index.js
+++ server/index.js
@@ -6,14 +6,31 @@ const connection = require("./db");
 const userRoutes = require("./routes/users");
 const postRoutes = require("./routes/posts");
 const authRoutes = require("./routes/auth");
-
+const multer = require("multer");
+const path = require("path");
 // database connection
 connection();
 
+app.use("/images",express.static(path.join(__dirname,"public/images")));
 // middlewares
 app.use(express.json());
 app.use(cors())
-
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"public/images");
+    },
+    filename:(req,file,cb)=>{
+        cb(null,req.body.name);
+    }
+});
+const upload = multer({storage});
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    try{
+return res.status(200).json("File uploaded successfully.");
+    }catch(err){
+console.log(err)
+    }
+});
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
