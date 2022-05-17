df57dbfdd7c95880a53e0d0e37c3a6b21d2942fc
messimaha <70766603+messimaha@users.noreply.github.com>
2022-05-17 10:04:28 +0200

    help

--- client/src/components/user/help/help.js
+++ client/src/components/user/help/help.js
@@ -1,14 +1,26 @@
 import emailjs from "emailjs-com"
+import React, { useState} from "react";
+import swal from 'sweetalert';
 import "./help.css"
 const Help = () =>{
+
     function sendEmail(e) {
         e.preventDefault();
         emailjs.sendForm('service_kg9614s', 'template_60e4wuc', e.target, '7gp0cBTF785jKUD53')
-        .then((result) => {
-        console.log(result.text);
-        }, (error) => {
-        console.log(error.text);
-        });
+        .then(
+
+          swal({
+            title: "Email Envoyer!",
+            icon: "success",
+          })
+         ) .catch(console.log("error")) 
+         
+        }
+        const [tags,setTages] = useState([])
+        function handelChange (e){
+         const value=e.target.value
+         if(!value.trim())
+         return setTages([...tags,value])
         }
     return (
         <div class="help">  
@@ -23,7 +35,7 @@ const Help = () =>{
             <input placeholder="Votre Email Address" name="user_email" type="email" tabindex="2" required/>
           </fieldset>
           <fieldset>
-            <textarea name="message" placeholder="Tapez Votre Message Ici...." tabindex="5" required/>
+            <textarea name="message" placeholder="Tapez Votre Message Ici...." tabindex="5" onchange={handelChange} required/>
           </fieldset>
           <fieldset>
             <button name="submit" type="submit" id="contact-submit">Send</button>
