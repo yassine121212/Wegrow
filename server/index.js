637374c48c2f4748b9504694f18ea0c3f8b53ebd
elouazzani yassine <80113396+yassine121212@users.noreply.github.com>
2022-05-17 23:54:55 +0100

    configure routes && shemas of conversations and messages

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
-
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 // database connection
 connection();
 
@@ -16,6 +17,7 @@ app.use(cors())
 // routes
 app.use("/api/users", userRoutes);
  app.use("/api/auth", authRoutes);
-
+ app.use("/api/conversations", conversationRoute);
+ app.use("/api/messages", messageRoute);
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
