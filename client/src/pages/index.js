1eb685d5fe19d0818bd84e5038a4b152dcdb2639
messimaha <70766603+messimaha@users.noreply.github.com>
2022-05-22 16:48:27 +0200

    Update index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,8 +3,8 @@ import "./style.css";
 import Footer from "../components/Footer/index";
 import Slider from "../components/Home/Slider/slid/Slider";
 import Service from "../components/Home/Services/Service";
-import Who from "../components/Home/who_we/who"
-import Feedback from "../components/feedback/feedback";
+import Who from "../components/Home/who_we/who";
+ import Feedback from "../components/feedback/feedback";
 
 const Home = () => {
      
@@ -14,7 +14,7 @@ const Home = () => {
                 <Slider />
                 <Service/>
                 <Feedback/>
-                <Who/>
+                 <Who/>
                 <Footer />
             </div>
         </div>
