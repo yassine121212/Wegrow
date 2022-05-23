8d20396d547c7f6ae567c3e06b45ddf346babced
elouazzani yassine <80113396+yassine121212@users.noreply.github.com>
2022-05-23 22:14:49 +0100

    Update multer-config.js

--- server/middleware/multer-config.js
+++ server/middleware/multer-config.js
@@ -17,4 +17,4 @@ const storage = multer.diskStorage({
   }
 });
 
-module.exports = multer({storage: storage}).single('proflilePicture');
+module.exports = multer({storage: storage}).single('profilePicture');
