afe785c6991ec3d6bbb4a57e952abef83b363a98
messimaha <70766603+messimaha@users.noreply.github.com>
2022-06-06 09:43:12 +0200

    traduire about

--- client/src/components/Home/who_we/who.js
+++ client/src/components/Home/who_we/who.js
@@ -29,7 +29,7 @@ const who = ()=>{
    return (
      <>
         <div id="about" className="lmp">
-       <div><h1 className='title_services' > <IoAlertCircleOutline></IoAlertCircleOutline><span> About</span>&nbsp;Us</h1></div> 
+       <div><h1 className='title_services' > <IoAlertCircleOutline></IoAlertCircleOutline><span> À propos</span>&nbsp;De nous</h1></div> 
        <div className="all">
 
            <div className="picdisc">
@@ -39,24 +39,24 @@ const who = ()=>{
            <div className="disc">
               <h1 className="tit1">WeGrow</h1>
                 <p className="par">
-             
-                New consumption models have emerged thanks to the rise of new 
-                technologies and changing lifestyles. Jobbing allows tens of 
-                thousands  of service providers to provide service in their 
-                neighborhood and earn additional income or launch their independent 
-                activity. <span className="work_color"><b>WeGrow</b></span> is a collaborative platform that is part of a 
-                fundamental trend: many sectors are experiencing their technological 
-                revolution such as transport with carpooling, car rental between 
-                individuals or housing. With <span className="work_color"><b>WeGrow</b></span>, the job market is undergoing a 
-                revolution! <ReadMore>The principle is simple: connect a "poster" (a person 
-                who needs a helping hand for services, kitchen installation, dressing
-                 room assembly, painting, parquet laying, tiling, moving, gardening)
-                  and a "provider" (person with the skills and time available) in 
-                  complete safety. An innovative model with the key to services 
-                  provided for those who do not have the time or the skills to do 
-                  everything themselves, and additional income and developing 
-                  know-how for service providers. They are students, retirees, 
-                  part-time employees, or people wishing to start their own business.</ReadMore></p>
+                De nouveaux modèles de consommation ont émergé grâce à l'essor de nouveaux
+                technologies et évolution des modes de vie. Le jobbing permet des dizaines de
+                des milliers de fournisseurs de services pour fournir des services dans leur
+                quartier et gagner un revenu complémentaire ou lancer leur indépendant
+                activité.
+             <span className="work_color"><b>WeGrow</b></span> est une plateforme collaborative faisant partie d'un
+                tendance de fond : de nombreux secteurs connaissent leur mutation technologique
+                révolution comme le transport avec covoiturage, la location de voiture entre
+                des particuliers ou des logements. Avec <span className="work_color"><b>WeGrow</b></span>, le marché du travail subit une
+                révolution! <ReadMore>Le principe est simple : connecter un "poster" (une personne
+                qui a besoin d'un coup de main pour les services, installation de cuisine, dressing
+                 montage de pièce, peinture, pose de parquet, carrelage, déménagement, jardinage)
+                  et un "prestataire" (personne disposant des compétences et du temps disponible) en
+                  sécurité complète. Un modèle innovant à la clé des services
+                  à ceux qui n'ont ni le temps ni les compétences pour faire
+                  tout eux-mêmes, et des revenus supplémentaires et en développement
+                  savoir-faire pour les prestataires de services. Ils sont étudiants, retraités,
+                  les salariés à temps partiel, ou les personnes souhaitant créer leur propre entreprise.</ReadMore></p>
                   
            </div>
        </div>
