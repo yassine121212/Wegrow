ccac27b5695ca2451bd070c199c4171058e17409
yassine121212 <elouazzaniyassine87@gmail.com>
2022-06-07 17:48:54 +0100

    maintenance

--- client/src/components/Home/How_to_use/How.js
+++ client/src/components/Home/How_to_use/How.js
@@ -2,28 +2,30 @@
 import "./How.css"
 import { IoAlertCircleOutline } from "react-icons/io5";
 import bal from "../../../images/Come back later.png";
+import ReactPlayer from 'react-player'
 
-const How = ()=>{
+const appvideo="https://www.youtube.com/watch?v=SMcztSqSQ18"
+ const How = ()=>{
    return (
      <>
          <div className="all1">
 
            <div className="disc1">
               <h1 className="tit2">Comment ça marche ?</h1>
-                <div className="par1">
+                <div className="par1" >
                   <div className="lign"><p>1. Chercher un <span className="erp">prestataire</span> et le <span className="erp">Contacter</span>.</p> </div>
                   <div className="lign"><p>2. Les prestataires <span className="erp">disponibles</span> et <span className="erp">compétents</span> vous proposent leurs <span className="erp"></span>services.</p> </div>
-                  <div className="lign"><p>3. <span className="erp">Evaluez</span> et <span className="erp">réglez</span> votre prestataire une fois que la<span className="erp">mission</span> est terminée.</p> </div>
+                  <div className="lign"><p>3. <span className="erp">Evaluez</span> et <span className="erp">réglez</span> votre prestataire une fois que la <span className="erp">mission</span> est terminée.</p> </div>
                 </div>
            </div>
-           <div className="picdisc">
-           <img className="whoimage" src={bal} alt="" />
+            <div className="picdisc">
+              <ReactPlayer
+                src={appvideo}
+                controls
+                className="vido"
 
-             <div className="gogo">
-                 </div>
- 
-
-           </div>
+              />
+            </div>
        </div>
       </>
    );
